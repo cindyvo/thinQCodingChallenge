eb83891ba3a037c02d1f8aec73ad5059db2f2dbb
Leonardo Ascenzi <lascenzi@colgate.edu>
2020-10-06 14:11:54 -0400

    testing server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+var request = require('request');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -8,3 +11,9 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT || 3000, function() {
   console.log('listening on *:3000');
 });
+
+request('https://superheroapi.com/api/3592560490806532', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+  }
+})
